1ad3bcab4c8b2a6ac0a77d618c71aee64a4f0809
philippe horne <phorne@uncharted.software>
2020-02-29 09:14:00 -0500

    Added a bit more logging.

--- task/dataset.go
+++ task/dataset.go
@@ -152,14 +152,17 @@ func augmentPredictionDataset(dataset *model.Dataset, variables []*cm.Variable) 
 
 // ClearDataset deletes the dataset and prediction data.
 func ClearDataset(pipelineID string, predictionID string) error {
+	log.Infof("clearing dataset and prediction info for pipeline '%s' and prediction '%s'", pipelineID, predictionID)
 	predictionsDir := env.ResolvePredictionPath(predictionID)
 	datasetDir := env.ResolveDatasetPath(predictionID)
 
 	// delete dataset directory & prediction directory
+	log.Infof("deleting prediction content found in '%s'", predictionsDir)
 	err := util.RemoveContents(predictionsDir)
 	if err != nil {
 		return err
 	}
+	log.Infof("deleting dataset content found in '%s'", datasetDir)
 	err = util.RemoveContents(datasetDir)
 	if err != nil {
 		return err
